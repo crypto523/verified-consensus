e17eea796d06c087459de88c6050953fe7c9d049
crypto523 <jacksoncurry46@gmail.com>
2022-10-29 13:33:36 +1100

    Basic types

---
+++ Types.thy
@@ -0,0 +1,89 @@
+theory Types
+  imports Main
+begin
+
+(* FIXME: implement overflow checks, etc *)
+datatype u8 = u8 nat
+datatype u64 = u64 nat
+
+datatype Slot = Slot u64
+datatype Epoch = Epoch u64
+
+datatype Hash256 = Hash256 "u8 list"
+
+(* FIXME: enforce length at type level? *)
+datatype 'a Vector = Vector "'a list"
+datatype 'a List = List "'a list"
+
+datatype Bitvector = Bitvector "bool list"
+
+datatype PublicKey = Pubkey "u8 list"
+
+datatype Version = Version "u8 list"
+
+record Fork =
+  previous_version :: Version
+  current_version :: Version
+  epoch :: Epoch
+
+record BeaconBlockHeader =
+  slot :: Slot
+  proposer_index :: u64
+  parent_root :: Hash256
+  state_root :: Hash256
+  body_root :: Hash256
+
+record Eth1Data =
+  deposit_root :: Hash256
+  deposit_count :: u64
+  block_hash :: Hash256
+
+record Validator =
+  pubkey :: PublicKey
+  withdrawal_credentials :: Hash256
+  slashed :: bool
+  activation_eligibility_epoch :: Epoch
+  activation_epoch :: Epoch
+  exit_epoch :: Epoch
+  withdrawable_epoch :: Epoch
+
+record ParticipationFlags =
+  head_flag :: bool
+  target_flag :: bool
+  source_flag :: bool
+
+record Checkpoint =
+  epoch :: Epoch
+  root :: Hash256
+
+record SyncCommittee =
+  pubkeys :: "PublicKey Vector"
+  aggregate_pubkey :: PublicKey
+
+record BeaconState =
+  genesis_time :: u64
+  genesis_validators_root :: Hash256
+  slot :: Slot
+  fork :: Fork
+  latest_block_header :: BeaconBlockHeader
+  block_roots :: "Hash256 Vector"
+  state_roots :: "Hash256 Vector"
+  historical_roots :: "Hash256 List"
+  eth1_data :: Eth1Data
+  eth1_data_votes :: "Eth1Data List"
+  eth1_deposit_index :: u64
+  validators :: "Validator List"
+  balances :: "u64 list"
+  randao_mixes :: "Hash256 Vector"
+  slashings :: "Hash256 Vector"
+  previous_epoch_participation :: "ParticipationFlags List"
+  current_epoch_participation :: "ParticipationFlags List"
+  justification_bits :: Bitvector
+  previous_justified_checkpoint :: Checkpoint
+  current_justified_checkpoint :: Checkpoint
+  finalized_checkpoint :: Checkpoint
+  inactivity_scores :: "u64 List"
+  current_sync_committee :: SyncCommittee
+  next_sync_committee :: SyncCommittee
+
+end
